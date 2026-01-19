aa1d1c4f108d3dd38a546b51d6dceb30297a21ba
desarrollador <desarrollador@DESKTOP-GHKU8SH>
2026-01-19 08:50:19 +0100

    Tercer commit de Javier Enríquez Camisón.

--- VersionesGithub/src/paquete/Clase.java
+++ VersionesGithub/src/paquete/Clase.java
@@ -4,14 +4,14 @@ public class Clase {
 
 	/*
     * Javier Enríquez
-    * @since 1.1
-    * @version 1.1
-    * Versión con una pequeña función.
+    * @since 1.2
+    * @version 1.2
+    * Versión con una función expandida.
     */
 	
 	static void primeraFuncion() {
-		int num = 0;
-		System.out.println(num);
+		int num1 = 6, num2 = 7;
+		System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
 	}
 	
 	public static void main(String[] args) {
