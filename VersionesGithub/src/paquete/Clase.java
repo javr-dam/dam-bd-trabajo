5703d284bf0756d951849b550605469cab22c7ab
desarrollador <desarrollador@DESKTOP-GHKU8SH>
2026-01-19 08:52:43 +0100

    Cuarto commit de Javier Enríquez Camisón.

--- VersionesGithub/src/paquete/Clase.java
+++ VersionesGithub/src/paquete/Clase.java
@@ -4,14 +4,17 @@ public class Clase {
 
 	/*
     * Javier Enríquez
-    * @since 1.2
-    * @version 1.2
-    * Versión con una función expandida.
+    * @since 1.3
+    * @version 1.3
+    * Versión con una función mas expandida.
     */
 	
 	static void primeraFuncion() {
 		int num1 = 6, num2 = 7;
 		System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+		System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
+		System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+		System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
 	}
 	
 	public static void main(String[] args) {
