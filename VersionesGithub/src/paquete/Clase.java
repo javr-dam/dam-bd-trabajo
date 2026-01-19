4a240b197a1557792d208c7af4b5efce0b8fbda4
desarrollador <desarrollador@DESKTOP-GHKU8SH>
2026-01-19 08:28:13 +0100

    Primer commit de Javier Enríquez Camisón.

--- VersionesGithub/src/paquete/Clase.java
+++ VersionesGithub/src/paquete/Clase.java
@@ -2,6 +2,16 @@ package paquete;
 
 public class Clase {
 
+	/*
+    * Javier Enríquez
+    * @since 1.0
+    * @version 1
+    * Versión inicial, sirve para comprobar si funciona.
+    */
+	static void primeraFuncion() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
