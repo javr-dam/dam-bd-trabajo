c2f369c5a60afb9ba897ad45a50287eb152b7a5d
desarrollador <desarrollador@DESKTOP-GHKU8SH>
2026-01-19 08:40:30 +0100

    Segundo commit de Javier Enríquez Camisón.

--- VersionesGithub/src/paquete/Clase.java
+++ VersionesGithub/src/paquete/Clase.java
@@ -4,17 +4,18 @@ public class Clase {
 
 	/*
     * Javier Enríquez
-    * @since 1.0
-    * @version 1
-    * Versión inicial, sirve para comprobar si funciona.
+    * @since 1.1
+    * @version 1.1
+    * Versión con una pequeña función.
     */
+	
 	static void primeraFuncion() {
-		
+		int num = 0;
+		System.out.println(num);
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		primeraFuncion();
 	}
 
 }
