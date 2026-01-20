66606924d4f3c844116147442a17af3b75d33d52
mdahmanih001-beep <mdahmanih001@iesarroyoharnina.es>
2026-01-20 12:11:36 +0100

    Version 1.5

--- VersionesGithub/src/paquete/Clase.java
+++ VersionesGithub/src/paquete/Clase.java
@@ -158,16 +158,17 @@ public class Clase {
 =======
 	/*
     * Merye Dahmani
-    * @since 1.4
-    * @version 1.4
-    * Mejora de las primeras verisones
+    * @since 1.5
+    * @version 1.5
+    * Se definen los numeros
 
     */
 	
         static void primeraFuncion() {
             Scanner sc = new Scanner(System.in);
-            
+            System.out.println("Introduce el primer numero");
             int num1  = sc.nextInt();
+            System.out.println("Introduce el segundo numero");
             
             int num2  = sc.nextInt();
             System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
