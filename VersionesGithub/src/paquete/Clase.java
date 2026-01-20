03a71f8b72db95b17744a612f35f3e435f439236
mdahmanih001-beep <mdahmanih001@iesarroyoharnina.es>
2026-01-20 12:19:57 +0100

    Version 1.6

--- VersionesGithub/src/paquete/Clase.java
+++ VersionesGithub/src/paquete/Clase.java
@@ -4,9 +4,9 @@ public class Clase {
 
 	/*
     * Merye Dahmani
-    * @since 1.5
-    * @version 1.5
-    * Se definen los numeros
+    * @since 1.6
+    * @version 1.6
+    * Realiza una operacion mas complicada
 
     */
 	
@@ -14,13 +14,21 @@ public class Clase {
             Scanner sc = new Scanner(System.in);
             System.out.println("Introduce el primer numero");
             int num1  = sc.nextInt();
-            System.out.println("Introduce el segundo numero");
             
+            System.out.println("Introduce el segundo numero");
             int num2  = sc.nextInt();
+
             System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
             System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
             System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
             System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
+
+            // División segura (evitar división por cero)
+            if( num != 0){
+                System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            }else{
+                System.out.printf("No se puede dividir entre cero");
+            }
         }
 
     public static void main(String[] args){
